9e63d951905245434566c148160aa0bc1d66961d
Timi Owoturo <owoturotimi@live.co.uk>
2020-04-29 19:02:28 -0400

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import * as firebase from 'firebase/app'
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: "AIzaSyCKjbrxmsRC1Tft64RU2eiqF1dM8rQ8CNM",
+    authDomain: "timiowourocom.firebaseapp.com",
+    databaseURL: "https://timiowourocom.firebaseio.com",
+    projectId: "timiowourocom",
+    storageBucket: "timiowourocom.appspot.com",
+    messagingSenderId: "964710175115",
+    appId: "1:964710175115:web:7c57a2576d7a3f5da1df8e"
+  }
 };
+firebase.initializeApp(environment.config);
